4c30061545459858bc0423f2939596d7c65af4c3
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-05-05 17:31:44 +0600

    Import core classes and exceptions in __init__.py

--- src/phystensor/core/__init__.py
+++ src/phystensor/core/__init__.py
@@ -1,1 +1,17 @@
+from phystensor.core.tensor import PhysicalTensor
+from phystensor.core.base import PhysicalBase
+from phystensor.core.dispatch import OpDispatcher
+from phystensor.core.exceptions import (
+    PhystensorError, 
+    DimensionalityError, 
+    UnitNotFoundError
+)
 
+__all__ = [
+    "PhysicalTensor",
+    "PhysicalBase",
+    "OpDispatcher",
+    "PhystensorError",
+    "DimensionalityError",
+    "UnitNotFoundError"
+]
