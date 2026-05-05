472d63d3de57dd84c12ecde24e8eedeb334a388a
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-05-05 17:37:50 +0600

    Add tests for quantity and complex string parsing

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,1 +1,10 @@
+import phystensor as pt
 
+def test_quantity_parsing():
+    q = pt.q(10, "kN")
+    assert q.data == 10000
+    assert q.dimensions.vector == (1, 1, -2, 0, 0, 0, 0)
+
+def test_complex_string_parsing():
+    q = pt.q(1, "kg*m/s^2")
+    assert q.dimensions.vector == (1, 1, -2, 0, 0, 0, 0)
