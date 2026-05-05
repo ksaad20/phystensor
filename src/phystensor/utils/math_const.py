a5ed94102adfcfe961caf4870790c78d927e23e1
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-05-05 17:06:38 +0600

    Add mathematical constants and conversion factors

---
+++ src/phystensor/utils/math_const.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Dimensionless constants (vector is all zeros)
+# These are used for EEE phase calcs and Maritime rotations.
+
+PI = np.pi
+TAU = 2 * np.pi
+E = np.e
+GOLDEN_RATIO = (1 + 5**0.5) / 2
+
+# Conversion factors for angles (Dimensionless)
+DEG_TO_RAD = PI / 180.0
+RAD_TO_DEG = 180.0 / PI
