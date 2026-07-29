7843d7b9b7bd7ba6ccec1b05e4bcecbc2684f440
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-07-29 22:05:52 +0600

    Update __init__.py

--- src/phystensor/utils/__init__.py
+++ src/phystensor/utils/__init__.py
@@ -1,9 +1,10 @@
-from phystensor.utils.parser import DimParser
-from phystensor.utils.formatting import PhysicsFormatter
-from phystensor.utils.math_const import PI, TAU, E, DEG_TO_RAD
 """phystensor.utils — utility subpackage."""
 from . import conversions
 from .conversions import TensorConverter
+from phystensor.utils.parser import DimParser
+from phystensor.utils.formatting import PhysicsFormatter
+from phystensor.utils.math_const import PI, TAU, E, DEG_TO_RAD
+
 
 __all__ = [
     "DimParser",
